0437f9d8bcf1ae49705a348ee6c340e4d5ad095b
Carlos55150233 <102787946+Carlos55150233@users.noreply.github.com>
2022-04-05 17:12:21 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = '382ee6b53bdd0df66a52ea9779c62424'
 # DATOS DEL MOODLE
 USUARIO = 'darian.borges@estudiantes.fbio.uh.cu'
 PASSWORD = 'darian1995'
-USUARIO_ID = 477147
+USUARIO_ID = 32581
 ZIP_MB = 100
 MOODLE_URL = 'evea.uh.cu'
 OWNER = [1291564836]
